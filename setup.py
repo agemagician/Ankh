8694db0ac336a030446ef8d8dc21b0838ea8dc6a
Ahmed Elnaggar <amit.najjar@gmail.com>
2022-11-21 16:31:05 +0100

    create setup.py
    create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Ankh',
+      version='0.00.0',
+      description='Optimized Protein Language Model ',
+      author='Ahmed Elnaggar',
+      author_email='ahmed.elnaggar@tum.de',
+      url='https://www.linkedin.com/in/prof-ahmed-elnaggar/',
+      packages=['distutils', 'distutils.command'],
+     )
