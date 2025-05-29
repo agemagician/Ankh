06a09692a7395045329fcca4dcfc1c3f0fb5c7dc
Hazem <hazemessam199@gmail.com>
2025-05-29 12:24:59 +0300

    removed warning function

--- src/ankh/models/utils.py
+++ src/ankh/models/utils.py
@@ -3,16 +3,6 @@ from functools import wraps
 from typing import List
 
 
-def deprecated_arg_warning(arg_name: str, arg_value: str, new_arg_name: str):
-    warnings.warn(
-        f"{arg_name} is deprecated and will be removed in the "
-        f"future. Please use `{new_arg_name}` instead. "
-        f"Received: {arg_value}",
-        DeprecationWarning,
-        stacklevel=3,
-    )
-
-
 def check_deprecated_args(deprecated_args: List[str], new_args: List[str]):
     """Decorator to check for deprecated arguments.
 
@@ -25,8 +15,12 @@ def check_deprecated_args(deprecated_args: List[str], new_args: List[str]):
         def _inner(*args, **kwargs):
             for deprecated_arg, new_arg in zip(deprecated_args, new_args):
                 if deprecated_arg in kwargs:
-                    deprecated_arg_warning(
-                        deprecated_arg, kwargs[deprecated_arg], new_arg
+                    warnings.warn(
+                        f"{deprecated_arg} is deprecated and will be removed "
+                        f"in the future. Please use `{new_arg}` instead. "
+                        f"Received: {kwargs[deprecated_arg]}",
+                        DeprecationWarning,
+                        stacklevel=2,
                     )
                     kwargs[new_arg] = kwargs[deprecated_arg]
                     del kwargs[deprecated_arg]
